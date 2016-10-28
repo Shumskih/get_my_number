ed3ba5aee5e035f0817f24a821c85c10b85c7078
Shumskih <music.dll@mail.ru>
2016-10-28 16:17:14 +0300

    update

---
+++ get_my_number.rb
@@ -0,0 +1,11 @@
+# Get My Number Game
+# Written by: Shumskih
+
+puts "Welcome to 'Get My Number!' game."
+print "Type your name: "
+
+username = gets.chomp!.to_s.to_sym
+
+puts "Welcome, #{username}!"
+
+ 
